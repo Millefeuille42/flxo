ff8116232e936dec8f4dd65c454ac985fb4f078b
Millefeuille <millefeuille42@proton.me>
2025-11-14 18:55:43 +0100

    feat(backend): rewrite presence router with service
    Signed-off-by: Millefeuille <millefeuille42@proton.me>

--- backend/flxo/routers/presence.py
+++ backend/flxo/routers/presence.py
@@ -2,69 +2,50 @@ from collections.abc import Sequence
 from datetime import datetime
 
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlmodel import select
 
+import flxo.services.presence as svc
 from flxo.models.presence import Presence, PresenceDTO, PresenceWithUser
 from flxo.models.user import UserPublic
 from flxo.services.auth import get_current_user
 from flxo.services.database import SessionDep
 
-from typing import Annotated, Optional
+from typing import Annotated
 
 router = APIRouter(prefix="/presence")
 
-# TODO Rewrite into service
 
 @router.get("/me", response_model=Sequence[PresenceWithUser])
 def list_self_presences(
     current_user: Annotated[UserPublic, Depends(get_current_user)],
     session: SessionDep,
-    start: Optional[datetime] = None,
-    end: Optional[datetime] = None,
+    start: datetime | None = None,
+    end: datetime | None = None,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
 ) -> Sequence[PresenceWithUser]:
-    query = select(Presence).where(Presence.user_id == current_user.id)
-
-    if start:
-        query = query.where(Presence.start >= start)
-    if end:
-        query = query.where(Presence.end <= end)
-
-    query = query.offset(offset).limit(limit)
-    return session.exec(query).all()
+    return svc.get_all_presences_of_user(  # type: ignore
+        session, current_user.id, start, end, offset, limit
+    )
 
 
 @router.get("/", response_model=Sequence[PresenceWithUser])
 def list_presences(
         _current_user: Annotated[UserPublic, Depends(get_current_user)],
         session: SessionDep,
-        start: Optional[datetime] = None,
-        end: Optional[datetime] = None,
+        start: datetime | None = None,
+        end: datetime | None = None,
         offset: int = 0,
         limit: Annotated[int, Query(le=100)] = 100,
 ) -> Sequence[PresenceWithUser]:
-    query = select(Presence)
-
-    if start:
-        query = query.where(Presence.start >= start)
-    if end:
-        query = query.where(Presence.end <= end)
-
-    query = query.offset(offset).limit(limit)
-    return session.exec(query).all()
+    return svc.get_all_presences(session, start, end, offset, limit)  # type: ignore
 
 
 @router.get("/{presence_id}", response_model=Presence)
-def get_presence(
+def get_presence_by_id(
     current_user: Annotated[UserPublic, Depends(get_current_user)],
     presence_id: int, session: SessionDep
 ) -> Presence:
-    presence = session.exec(
-        select(Presence)
-        .where(Presence.id == presence_id)
-        .where(Presence.user_id == current_user.id)
-    ).first()
+    presence = svc.get_presence(session, presence_id, current_user.id)
     if not presence:
         raise HTTPException(status_code=404, detail="presence not found")
     return presence
@@ -75,15 +56,9 @@ def create_presence(
     current_user: Annotated[UserPublic, Depends(get_current_user)],
     presence: PresenceDTO, session: SessionDep
 ) -> Presence:
-    overlaps = session.exec(
-        select(Presence)
-        .where(Presence.user_id == current_user.id)
-        .where(
-            (Presence.start < presence.end)
-            & (Presence.end > presence.start)
-        )
-    ).first()
-    if overlaps:
+    if svc.does_presence_overlap(
+        session, current_user.id, presence.start, presence.end
+    ):
         raise HTTPException(
             status_code=400, detail="Presence overlaps with an existing one"
         )
@@ -93,53 +68,29 @@ def create_presence(
             status_code=400, detail="start must be after current date"
         )
 
-    db_presence = Presence(
-        user_id=current_user.id,
-        start=presence.start,
-        end=presence.end,
-    )
-    session.add(db_presence)
-    session.commit()
-    session.refresh(db_presence)
-    return db_presence
+    return svc.create_presence(session, presence, current_user.id)
 
 
 @router.put("/{presence_id}", response_model=Presence)
 def update_presence(
         current_user: Annotated[UserPublic, Depends(get_current_user)],
         presence_id: int,
-        presence_update: PresenceDTO,
+        presence_dto: PresenceDTO,
         session: SessionDep
 ) -> Presence:
-    presence = session.exec(
-        select(Presence)
-        .where(Presence.id == presence_id)
-        .where(Presence.user_id == current_user.id)
-    ).first()
-
+    presence = svc.get_presence(session, presence_id, current_user.id)
     if not presence:
         raise HTTPException(status_code=404, detail="Presence not found")
 
-    overlap = session.exec(
-        select(Presence)
-        .where(Presence.user_id == current_user.id)
-        .where(Presence.id != presence_id)
-        .where(
-            (Presence.start < presence_update.end)
-            & (Presence.end > presence_update.start)
+    if svc.does_presence_overlap(
+        session, current_user.id, presence_dto.start, presence_dto.end, presence.id
+    ):
+        raise HTTPException(
+            status_code=400,
+            detail="Updated presence overlaps with an existing one"
         )
-    ).first()
 
-    if overlap:
-        raise HTTPException(status_code=400, detail="Updated presence overlaps with an existing one")
-
-    presence.start = presence_update.start
-    presence.end = presence_update.end
-
-    session.add(presence)
-    session.commit()
-    session.refresh(presence)
-    return presence
+    return svc.update_presence(session, presence_dto, presence)
 
 
 @router.delete("/{presence_id}")
@@ -147,13 +98,8 @@ def delete_presence(
     current_user: Annotated[UserPublic, Depends(get_current_user)],
     presence_id: int, session: SessionDep
 ):
-    presence = session.exec(
-        select(Presence)
-        .where(Presence.id == presence_id)
-        .where(Presence.user_id == current_user.id)
-    ).first()
+    presence = svc.get_presence(session, presence_id, current_user.id)
     if not presence:
         raise HTTPException(status_code=404, detail="presence not found")
-    session.delete(presence)
-    session.commit()
+    svc.delete_presence(session, presence)
     return {"ok": True}
