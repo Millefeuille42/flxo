4e5d6b9dd6a89f7719ffc16e04be56ce3d0664ce
Millefeuille <millefeuille42@proton.me>
2026-01-14 17:45:15 +0100

    feat: add reverse proxy support

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
+
 /app/.venv/bin/alembic upgrade head
-/app/.venv/bin/uvicorn flxo:app --host 0.0.0.0 --port 80 $@
+
+if [ "$FLXO__REVERSE_PROXY" = "true" ]; then
+  echo "Starting with reverse proxy settings"
+  /app/.venv/bin/uvicorn flxo:app --proxy-headers --forwarded-allow-ips "*" --host 0.0.0.0 --port 80 $@
+else
+  echo "Starting without reverse proxy settings"
+  /app/.venv/bin/uvicorn flxo:app --host 0.0.0.0 --port 80 $@
+fi
+
