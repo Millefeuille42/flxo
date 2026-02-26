a327608a7e3f8a8fbc21a46125e8b99122484a0e
Millefeuille <millefeuille42@proton.me>
2026-02-26 18:38:27 +0100

    feat: add seat overlap detection

--- backend/flxo/models/presence.py
+++ backend/flxo/models/presence.py
@@ -39,14 +39,13 @@ class PresenceBase(SQLModel):
 
 
 class PresenceDTO(PresenceBase):
-    pass
+    seat_id: int = Field(default=0, foreign_key="seat.id")
 
 
-class Presence(PresenceBase, table=True):
+class Presence(PresenceDTO, table=True):
     id: int | None = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     office_id: int | None = Field(default=None, foreign_key="office.id")
-    seat_id: int | None = Field(default=None, foreign_key="seat.id")
 
     user: "User" = Relationship(back_populates="presences")
     office: "Office" = Relationship(back_populates="presences")
