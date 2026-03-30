41517808ce4251f8f5a229d9595cbe3d18487e55
Emmanuel <me@guzu.net>
2026-03-30 13:50:44 +0200

    fixup! feat(frontend): desk picker modal, floor plan and user colors

--- frontend/src/components/WeekGrid.vue
+++ frontend/src/components/WeekGrid.vue
@@ -2,7 +2,7 @@
 import { computed, ref } from 'vue'
 import PersonRow from './PersonRow.vue'
 import DeskPickerModal from './DeskPickerModal.vue'
-import { sortedPersons, bookings, activeOfficeId, currentWeekOffset, getWeekKey, getWeekDates, getTodayDayIndex, setSlotDesk, weekKeyDayToISO, deskCount } from '../state.js'
+import { sortedPersons, bookings, activeOfficeId, currentWeekOffset, getWeekKey, getWeekDates, getTodayDayIndex, setSlotDesk, weekKeyDayToISO, deskCount, hoveredSlot } from '../state.js'
 
 const DAY_NAMES = ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven']
 const DAYS = [0, 1, 2, 3, 4]
@@ -174,6 +174,8 @@ const weeks = computed(() => {
                 }]"
                 :style="deskCellStyle(week, di, slot)"
                 @click="canPickDesk(week, di, slot) && openDeskPicker(week, di, slot)"
+                @mouseenter="getLoggedBooking(week.weekKey, di, slot) && (hoveredSlot = { weekKey: week.weekKey, day: di, slot })"
+                @mouseleave="hoveredSlot = null"
               >
                 <div v-if="getLoggedBooking(week.weekKey, di, slot)" class="desk-slot-inner">
                   <svg viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="desk-slot-icon">
